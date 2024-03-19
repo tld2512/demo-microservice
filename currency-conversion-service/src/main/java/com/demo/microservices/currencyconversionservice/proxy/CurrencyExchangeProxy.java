7350cf697290d345541efaf6af480766e70443af
tld2512 <tienlongdinh251297@gmail.com>
2024-03-19 20:59:12 +0700

    configure naming server

---
+++ currency-conversion-service/src/main/java/com/demo/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,12 @@
+package com.demo.microservices.currencyconversionservice.proxy;
+
+import com.demo.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
